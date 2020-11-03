3c45355dc2f43ce2406ad8f30535aef777f613b7
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-03 14:23:50 +0530

    add 03_11.py

---
+++ 03_11.py
@@ -0,0 +1,14 @@
+# Consecutive Characters
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_power, count = 1, 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                count += 1
+                if count > max_power:
+                    max_power = count
+            else:
+                count = 1
+        return max_power
+        
