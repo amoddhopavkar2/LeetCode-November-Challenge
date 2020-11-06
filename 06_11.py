9393e2ad1a7a226ea6d2c2425e102e3b948519f1
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-06 16:35:41 +0530

    add 06_11.py

---
+++ 06_11.py
@@ -0,0 +1,14 @@
+#  Find the Smallest Divisor Given a Threshold
+
+class Solution:
+	def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+		left, right = 1, max(nums)
+
+		while left <= right:
+			mid = (left + right) // 2
+			if sum(math.ceil(num / mid) for num in nums) > threshold:
+				left = mid + 1
+			else:
+				right = mid - 1
+
+		return left
