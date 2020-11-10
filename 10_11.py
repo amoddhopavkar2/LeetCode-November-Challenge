d8cf18912860fa87777311b2bca322abd9072715
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-10 14:07:05 +0530

    add 10_11.py

---
+++ 10_11.py
@@ -0,0 +1,11 @@
+# Flipping an Image
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for row in A:
+            i, j = 0, len(row) - 1
+            while i <= j:
+                row[i] , row[j] , i , j = 1^row[j] , 1^row[i] , i+1 , j-1
+                
+        return A
+        
