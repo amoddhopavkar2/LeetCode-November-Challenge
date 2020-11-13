2fe724f1f5987da5403ad649f101d9d4a7c7a184
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-14 02:50:07 +0530

    update 13_11.py

--- 13_11.py
+++ 13_11.py
@@ -1,7 +1,7 @@
 # Populating Next Right Pointers in Each Node
 
 """
-# Definition for a Node.
+Definition for a Node.
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
