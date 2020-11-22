18747b37ff3b3a499709391e0757bc1178a8ff60
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-22 14:53:10 +0530

    add 22_11.py

--- 21_11.py
+++ 21_11.py
@@ -1,1 +1,22 @@
 # Numbers At Most N Given Digit Set
+
+class Solution:
+    def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
+        digits = set(map(int, digits))
+        sn = str(n)
+        ln = len(sn)
+
+        cnt = 0
+
+        for l in range(1, ln):
+            cnt += len(digits) ** l
+
+        for i, dn in enumerate(sn):
+            dn = int(dn)
+            less_than = sum(d < dn for d in digits)
+            cnt += less_than * len(digits) ** (ln - i - 1)
+            if dn not in digits: break
+            elif i == ln - 1: cnt += 1
+
+        return cnt
+        
---
+++ 22_11.py
@@ -0,0 +1,14 @@
+# Unique Morse Code Words
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        result = []
+        for word in words:
+            morse = ''
+            for char in word:
+                morse += morse_code[ord(char) - 97]
+            result.append(morse)
+        return (len(set(result)))
+        
