a27b6330fe0d057d4e784a1a3cf4fb4b9c270ec6
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-27 16:15:38 +0530

    add 27_11.py

---
+++ 27_11.py
@@ -0,0 +1,25 @@
+#  Partition Equal Subset Sum
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 == 1:
+            return False
+
+        target = sum(nums) // 2
+        dp = [0] * (target * 2)
+        dp[0] = 1
+        for num in nums:
+            if num > target:
+                return False
+            
+            if num == target:
+                return True
+            
+            for i in range(target, -1, -1):
+                if dp[i] == 1:
+                    dp[i+num] = 1
+                
+                if dp[target]:
+                    return True
+        
+        return False
