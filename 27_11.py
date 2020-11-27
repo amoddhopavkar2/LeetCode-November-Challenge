a8c26d37d2d237bdb524f0bf2e548ac1b29d68a3
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-11-27 16:16:12 +0530

    update 27_11.[y

--- 27_11.py
+++ 27_11.py
@@ -10,16 +10,15 @@ class Solution:
         dp[0] = 1
         for num in nums:
             if num > target:
-                return False
-            
+                return False 
             if num == target:
                 return True
             
             for i in range(target, -1, -1):
                 if dp[i] == 1:
                     dp[i+num] = 1
-                
                 if dp[target]:
                     return True
         
         return False
+        
