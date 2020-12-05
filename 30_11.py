9d6486b0e4879b6f635bd0302bb7e0324ae74ea4
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-12-05 15:43:37 +0530

    update 30_11.py

--- 30_11.py
+++ 30_11.py
@@ -2,8 +2,8 @@
 
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        line = sorted((pos,tag,height,counterpart) for l,r,h in buildings 
-                    for pos,tag,height,counterpart in [(l,'l',-h,r),(r,'r',-h,l)])
+        line = sorted((pos,tag,height,counterpart) for l, r, h in buildings 
+                    for pos,tag,height,counterpart in [(l, 'l', -h, r), (r, 'r', -h, l)])
         prev_h, skyline, h_index = 0, [], [(0, 1<<32)]
         for p,t,h,c in line:
             if t == 'l':
